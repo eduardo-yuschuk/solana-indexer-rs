25bca5443e6971bfd0191710beb79b84876555d8
Eduardo Yuschuk <eduardo.yuschuk@gmail.com>
2025-02-17 00:05:33 -0300

    -

--- block_indexer/src/main.rs
+++ block_indexer/src/main.rs
@@ -1,5 +1,8 @@
 // use as reference: js_src/src/fetching/block-indexer.ts
 
+// enable unused vars
+#![allow(unused)]
+
 use flate2::read::GzDecoder;
 use postgres::Client;
 use serde_json;
@@ -49,7 +52,7 @@ fn main() {
             println!("Slot: {:?}", slot);
             println!("Block time: {:?}", block_time);
 
-            let events = parse_block(&data_obj);
+            let events = parse_block(slot, &data_obj);
 
             let parse_time = begin.elapsed();
             println!("Parse time: {:?}", parse_time);
@@ -75,15 +78,42 @@ fn main() {
 #[derive(Debug, Clone)]
 struct Event {}
 
-fn parse_block(data_obj: &serde_json::Value) -> Vec<Event> {
+fn parse_block(slot: i32, data_obj: &serde_json::Value) -> Vec<Event> {
     let mut events: Vec<Event> = Vec::new();
+
+    let block_height = data_obj.get("blockHeight").unwrap().as_i64().unwrap();
+    let block_time = data_obj.get("blockTime").unwrap().as_i64().unwrap();
+    let block_hash = data_obj.get("blockhash").unwrap().as_str().unwrap();
+    let parent_slot = data_obj.get("parentSlot").unwrap().as_i64().unwrap() as i32;
+    let previous_block_hash = data_obj.get("previousBlockhash").unwrap().as_str().unwrap();
+
+    println!("block_height: {:?}", block_height);
+    println!("block_time: {:?}", block_time);
+    println!("block_hash: {:?}", block_hash);
+    println!("parent_slot: {:?}", parent_slot);
+    println!("previous_block_hash: {:?}", previous_block_hash);
 
     let transactions = data_obj.get("transactions").unwrap();
 
-    for transaction in transactions.as_array().unwrap() {
-        let transaction_obj: &serde_json::Map<String, serde_json::Value> =
-            transaction.as_object().unwrap();
-        parse_transaction(transaction_obj)
+    for transaction_obj in transactions.as_array().unwrap() {
+        let meta = transaction_obj.get("meta").unwrap();
+        let compute_units_consumed = meta.get("computeUnitsConsumed").unwrap().as_i64().unwrap();
+        let err = meta.get("err").unwrap();
+        let fee = meta.get("fee").unwrap().as_i64().unwrap();
+        let inner_instructions = meta.get("innerInstructions").unwrap();
+        let loaded_addresses = meta.get("loadedAddresses").unwrap();
+        let log_messages = meta.get("logMessages").unwrap();
+        let post_balances = meta.get("postBalances").unwrap();
+        let pre_balances = meta.get("preBalances").unwrap();
+        let post_token_balances = meta.get("postTokenBalances").unwrap();
+        let pre_token_balances = meta.get("preTokenBalances").unwrap();
+        let rewards = meta.get("rewards").unwrap();
+        let status = meta.get("status").unwrap();
+
+        let transaction: &serde_json::Map<String, serde_json::Value> =
+            transaction_obj.as_object().unwrap();
+        
+        parse_transaction(slot, block_time, transaction)
             .iter()
             .for_each(|event| events.push(event.clone()));
     }
@@ -96,7 +126,11 @@ fn parse_block(data_obj: &serde_json::Value) -> Vec<Event> {
 //     fn parse_transaction(&self, data_obj: &serde_json::Value) -> Vec<Event>;
 // }
 
-fn parse_transaction(transaction_obj: &serde_json::Map<String, serde_json::Value>) -> Vec<Event> {
+fn parse_transaction(
+    slot: i32,
+    block_time: i64,
+    transaction_obj: &serde_json::Map<String, serde_json::Value>,
+) -> Vec<Event> {
     let mut events = Vec::new();
     let moonshot_parser = MoonshotParser::new();
 
@@ -124,4 +158,22 @@ fn save_events(_events: &Vec<Event>) -> (i32, HashMap<String, Vec<Event>>) {
 fn build_events_report(_events_by_source_and_type: &HashMap<String, Vec<Event>>) -> String {
     let report = String::new();
     report
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::File;
+    use std::io::BufReader;
+    
+    #[test]
+    fn test_parse_transaction() {
+        // read tx.json
+        let file = File::open("data/moon_tx.json").unwrap();
+        let reader = BufReader::new(file);
+        let data_obj: serde_json::Value = serde_json::from_reader(reader).unwrap();
+
+        let events = parse_transaction(1, 1739711240, &data_obj);
+        println!("events: {:?}", events);
+    }    
 }
--- block_indexer/src/moonshot.rs
+++ block_indexer/src/moonshot.rs
@@ -1,6 +1,7 @@
 use crate::Event;
 use serde_json::Value;
-
+use solana_sdk::bs58;
+use num_bigint::BigUint;
 pub struct MoonshotParser;
 
 impl MoonshotParser {
@@ -8,13 +9,147 @@ impl MoonshotParser {
         MoonshotParser
     }
 
-    pub fn parse_instruction(&self, _instruction: &Value) -> Vec<Event> {
+    pub fn parse_instruction(&self, instruction: &Value) -> Vec<Event> {
         let _events = Vec::new();
 
         // Aquí iría la lógica para parsear instrucciones específicas de Moonshot
         // Por ahora retornamos un vector vacío
 
+        let instruction_data = instruction.get("data").unwrap();
+        let instruction_data_str = instruction_data.as_str().unwrap();
+        let instruction_data_bytes = bs58::decode(instruction_data_str).into_vec().unwrap();
+
+        let decoded_instruction = decode_instruction_data(&instruction_data_bytes);
+
+        match decoded_instruction {
+            MoonshotInstructionData::Trade(_trade) => {
+                // TODO: Implement buy event
+            }
+            MoonshotInstructionData::TokenMint(_token_mint) => {
+                // TODO: Implement sell event
+            }
+            MoonshotInstructionData::Unknown => {
+                // TODO: Implement unknown event
+            }
+        }
+
         _events
+    }
+}
+
+enum MoonshotInstructionDiscriminator {
+    Buy,
+    Sell,
+    TokenMint,
+    Unknown,
+}
+
+impl MoonshotInstructionDiscriminator {
+    fn from_u64(value: u64) -> Self {
+        match value {
+            16927863322537952870 => MoonshotInstructionDiscriminator::Buy,
+            12502976635542562355 => MoonshotInstructionDiscriminator::Sell,
+            12967285527113116675 => MoonshotInstructionDiscriminator::TokenMint,
+            _ => MoonshotInstructionDiscriminator::Unknown,
+        }
+    }
+}
+
+enum MoonshotInstructionData {
+    Trade(MoonshotTradeValues),
+    TokenMint(MoonshotTokenMintValues),
+    Unknown,
+}
+
+pub struct MoonshotTradeValues {
+    pub token_amount: u64,
+    pub collateral_amount: u64,
+    pub fixed_side: u8,
+    pub slippage_bps: u64,
+}
+
+pub struct MoonshotTokenMintValues {
+    pub name: String,
+    pub symbol: String,
+    pub uri: String,
+    pub decimals: u8,
+    pub collateral_currency: u8,
+    pub amount: u64,
+    pub curve_type: u8,
+    pub migration_target: u8,
+}
+
+fn u64_bytes_to_big_int(bytes: &[u8], offset: usize) -> BigUint {
+    let mut value = BigUint::ZERO;
+    value = value | BigUint::from(bytes[offset + 7]) << 56;
+    value = value | BigUint::from(bytes[offset + 6]) << 48;
+    value = value | BigUint::from(bytes[offset + 5]) << 40;
+    value = value | BigUint::from(bytes[offset + 4]) << 32;
+    value = value | BigUint::from(bytes[offset + 3]) << 24;
+    value = value | BigUint::from(bytes[offset + 2]) << 16;
+    value = value | BigUint::from(bytes[offset + 1]) << 8;
+    value = value | BigUint::from(bytes[offset + 0]);
+    value
+}
+
+fn decode_instruction_data(instruction_data: &[u8]) -> MoonshotInstructionData {
+    // el primer u64 es el discriminador
+    let instruction_type = u64_bytes_to_big_int(instruction_data, 0);
+
+    println!("instruction_type: {}", instruction_type);
+
+    // match MoonshotInstructionDiscriminator::from_u64(instruction_type) {
+    //     MoonshotInstructionDiscriminator::Buy => {
+    //         let decoded_trade = decode_trade(instruction_data);
+    //         MoonshotInstructionData::Trade(decoded_trade)
+    //     }
+    //     MoonshotInstructionDiscriminator::Sell => {
+    //         let decoded_trade = decode_trade(instruction_data);
+    //         MoonshotInstructionData::Trade(decoded_trade)
+    //     }
+    //     MoonshotInstructionDiscriminator::TokenMint => {
+    //         let decoded_token_mint = decode_token_mint(instruction_data);
+    //         MoonshotInstructionData::TokenMint(decoded_token_mint)
+    //     }
+    //     MoonshotInstructionDiscriminator::Unknown => todo!(),
+    // }
+
+    MoonshotInstructionData::Unknown
+}
+
+fn decode_trade(instruction_data: &[u8]) -> MoonshotTradeValues {
+    let token_amount = u64::from_le_bytes(instruction_data[8..16].try_into().unwrap());
+    let collateral_amount = u64::from_le_bytes(instruction_data[16..24].try_into().unwrap());
+    let fixed_side = u8::from_le_bytes(instruction_data[24..25].try_into().unwrap());
+    let slippage_bps = u64::from_le_bytes(instruction_data[25..33].try_into().unwrap());
+
+    MoonshotTradeValues {
+        token_amount,
+        collateral_amount,
+        fixed_side,
+        slippage_bps,
+    }
+}
+
+fn decode_token_mint(instruction_data: &[u8]) -> MoonshotTokenMintValues {
+    let name = String::from_utf8(instruction_data[8..].to_vec()).unwrap();
+    let symbol = String::from_utf8(instruction_data[16..].to_vec()).unwrap();
+    let uri = String::from_utf8(instruction_data[24..].to_vec()).unwrap();
+    let decimals = u8::from_le_bytes(instruction_data[32..33].try_into().unwrap());
+    let collateral_currency = u8::from_le_bytes(instruction_data[33..34].try_into().unwrap());
+    let amount = u64::from_le_bytes(instruction_data[34..42].try_into().unwrap());
+    let curve_type = u8::from_le_bytes(instruction_data[42..43].try_into().unwrap());
+    let migration_target = u8::from_le_bytes(instruction_data[43..44].try_into().unwrap());
+
+    MoonshotTokenMintValues {
+        name,
+        symbol,
+        uri,
+        decimals,
+        collateral_currency,
+        amount,
+        curve_type,
+        migration_target,
     }
 }
 
